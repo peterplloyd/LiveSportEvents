8252d15efd76b474692e4b1e2a5773b2f83ca93c
peterplloyd <hello@peterplloyd.co.uk>
2022-08-15 20:16:13 +0100

    Add a footer

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import styles from './footer.module.scss';
+
+const Footer: React.FC = () => {
+	return (
+		<footer className={styles.footer}>
+			<div className={styles.footer__container}>
+				<p>
+					&#169; 2022 Bonne Terre Limited or its affiliated companies. The Sky
+					trademarks are owned by Sky UK Limited and are used under licence. All
+					rights reserved.
+				</p>
+			</div>
+		</footer>
+	);
+};
+
+export default Footer;
